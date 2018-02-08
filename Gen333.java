764d065ef9de9756a51ef4d13757448d489a6e4d
yomi <yojuolape@gmail.com>
2018-02-07 20:19:28 -0800

    Just go
    Just Go!

--- Gen333.java
+++ Gen333.java
@@ -1,13 +1,13 @@
 public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
-			System.out.println(&quot;You&#39;re awesome&quot;);
+			System.out.println("Youre awesome");
 		else if (score &gt; 90)
-			System.out.println(&quot;That&#39;s great&quot;);
+			System.out.println("That's great");
 		else if (score &gt; 60)
-			System.out.println(&quot;That&#39;s good &quot;);
+			System.out.println("That's good");
 		else
-			System.out.println(&quot;Well, what can I say?&quot;);
+			System.out.println("Well, what can I say?");
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
