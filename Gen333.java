a450fc1a73f30dd0e23e0ec830c621f5878417ca
oojuolape <oojuolape@stu.jsu.edu>
2018-02-07 22:24:39 -0800

    Update Gen333.java

--- Gen333.java
+++ Gen333.java
@@ -2,9 +2,9 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("Youre awesome");
-		else if (score &gt; 90)
+		else if (score > 90)
 			System.out.println("That's great");
-		else if (score &gt; 60)
+		else if (score > 60)
 			System.out.println("That's good");
 		else
 			System.out.println("Well, what can I say?");
